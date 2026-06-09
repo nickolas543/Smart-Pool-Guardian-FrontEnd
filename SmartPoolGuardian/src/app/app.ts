eebf962811db8ca75cd67eff973a981beca86569
nickolas543 <prevencione2@hotmail.com>
2026-06-09 17:13:50 -0500

    Correcion de problema en app.ts

--- SmartPoolGuardian/src/app/app.ts
+++ SmartPoolGuardian/src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {
-  title = 'SmartPoolGuardian';
+export class App {
+  protected readonly title = signal('SmartPoolGuardian');
 }
+
